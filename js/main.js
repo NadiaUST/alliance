b7d34fb4499ad655b708e9bd7c988d6ace161fb3
NadiaUST <ustinova7n@gmail.com>
2025-08-17 13:11:22 -0500

    add: js file

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const navbar = document.querySelector(".navbar") //обращение к неизм.константе, чтобы найти класс навбар и поместить в переменную навбар
+
+window.addEventListener("scroll", () => { //когда пользователь начнет прокрутку страницы, вызовет действие:
+  if (this.scrollY > 1) { //если это окно прокручивается по оси Y > 1 раза, то добавь класс navbar-light
+    navbar.classList.add("navbar-light");
+  } else { // если прокрутили обратно наверх, то удали класс navbar-light
+    navbar.classList.remove("navbar-light");
+  }
+}) 
