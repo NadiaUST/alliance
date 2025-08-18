74e35cc6ecd0c89353a185775dedc488160b7a19
NadiaUST <ustinova7n@gmail.com>
2025-08-18 14:32:19 -0500

    fix: icons svg sprite

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
 const navbar = document.querySelector(".navbar"); //обращение к неизм.константе, чтобы найти класс навбар и поместить в переменную навбар
+const logo = document.querySelector(".logo-svg use"); // создаем константу logo
 
 window.addEventListener("scroll", () => {
   //когда пользователь начнет прокрутку страницы, вызовет действие:
   if (this.scrollY > 1) {
     //если это окно прокручивается по оси Y > 1 раза, то добавь класс navbar-light
     navbar.classList.add("navbar-light");
+    logo.href.baseVal = "img/sprite.svg#logo"; // меняем baseVal на другой адрес, нашего svg
   } else {
     // если прокрутили обратно наверх, то удали класс navbar-light
     navbar.classList.remove("navbar-light");
+    logo.href.baseVal = "img/sprite.svg#logo_light";
   }
 });
