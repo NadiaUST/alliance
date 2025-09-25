07fe6e5668a223bc3f4abcad62eb63018122010a
NadiaUST <ustinova7n@gmail.com>
2025-09-25 13:31:44 -0500

    create: about page

--- about.php
+++ about.php
@@ -3,5 +3,7 @@ $page_title = 'О компании';
 $header_style = 'about-company';
 $swiper_color = 'swiper-color';
 include_once('header-page.php');
-include_once('./template-parts/swiper-block.php'); ?>
+include_once('./template-parts/swiper-block.php');
+include_once('./template-parts/founder-block.php');
+include_once('./template-parts/clients-block.php'); ?>
 <?php include_once('footer.php');?>
