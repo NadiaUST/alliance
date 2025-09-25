7986f8dd8a2e40d083d7e649a7c4da70096c12d1
NadiaUST <ustinova7n@gmail.com>
2025-09-25 13:39:15 -0500

    create: added page php

---
+++ template-parts/swiper-light-block.php
@@ -0,0 +1,67 @@
+<section class="section section-light">
+      <div class="container">
+        <div class="seporator seporator-two"></div>
+        <h2 class="section-title">схема работы</h2>
+        <!-- Слайдер -->
+        <div class="swiper swiperSteps">
+          <!-- Обложка слайдера -->
+          <ol class="swiper-wrapper steps">
+            <!-- Слайды -->
+            <li class="swiper-slide steps-item">
+              <span class="steps-num">01</span>
+              <h3 class="steps-title">Знакомство</h3>
+              <p class="steps-text">
+                Безусловно, сплочённость команды профессионалов позволяет
+                оценить значение форм воздействия.
+              </p>
+              <a href="#" class="button-link">Оставить заявку</a>
+            </li>
+            <li class="swiper-slide steps-item">
+              <span class="steps-num">02</span>
+              <h3 class="steps-title">
+                Заключение <br />
+                договора
+              </h3>
+              <p class="steps-text">
+                Лишь интерактивные прототипы призваны к ответу.
+              </p>
+            </li>
+            <li class="swiper-slide steps-item">
+              <span class="steps-num">03</span>
+              <h3 class="steps-title">Производство</h3>
+              <p class="steps-text">
+                А также стремящиеся вытеснить традиционное производство,
+                нанотехнологии функционально разнесены на независимые <br />
+                элементы.
+              </p>
+            </li>
+            <li class="swiper-slide steps-item">
+              <span class="steps-num">04</span>
+              <h3 class="steps-title">Доставка</h3>
+              <p class="steps-text">
+                В частности, экономическая повестка сегодняшнего дня говорит о
+                возможностях приоритизации <br />
+                разума над эмоциями.
+              </p>
+            </li>
+          </ol>
+          <!-- Кнопки навигации -->
+          <div class="sliderSteps-buttons primary-buttons-wrapper">
+            <div class="sliderSteps-button-prev primary-button-prev">
+              <svg width="36" height="24">
+                <use href="img/sprite.svg#arrow-prev"></use>
+              </svg>
+            </div>
+            <div class="sliderSteps-button-next primary-button-next">
+              <svg width="36" height="24">
+                <use href="img/sprite.svg#arrow-next"></use>
+              </svg>
+            </div>
+          </div>
+          <!-- /.slider-buttons -->
+        </div>
+        <!-- swiper -->
+      </div>
+      <!-- /.container -->
+    </section>
+    <!-- /.section-light -->
