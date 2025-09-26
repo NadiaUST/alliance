61e36db4c1c4c451e9a59413565909dbd62268c8
NadiaUST <ustinova7n@gmail.com>
2025-09-26 14:36:11 -0500

    create: added adaptive for section about

--- about.php
+++ about.php
@@ -6,6 +6,7 @@ $header_margin = 'header-margin';
 include_once('header-page.php');
 include_once('./template-parts/expert-block.php');
 include_once('./template-parts/swiper-block.php');
+include_once('./template-parts/our-production-block.php');
 include_once('./template-parts/founder-block.php');
 include_once('./template-parts/clients-block.php'); ?>
 <?php include_once('footer.php');?>
---
+++ template-parts/our-production-block.php
@@ -0,0 +1,66 @@
+<section class="section expert">
+      <div class="container">
+        <div class="expert-content-wrapper">
+          <div class="expert-content">
+            <div class="seporator"></div>
+            <h2 class="expert-title">Наше производство</h2>
+            <p class="expert-text">
+             Предварительные выводы неутешительны: разбавленное изрядной долей эмпатии, рациональное мышление обеспечивает широкому кругу (специалистов) участие в формировании глубокомысленных рассуждений. Но граница обучения кадров создаёт необходимость включения в производственный план целого ряда внеочередных мероприятий с учётом комплекса кластеризации усилий.
+            </p>
+            <p class="expert-text">
+              Реализация намеченных плановых заданий, а также свежий взгляд на привычные вещи - безусловно открывает новые горизонты для соответствующих условий активизации. Предварительные выводы неутешительны: экономическая повестка сегодняшнего дня требует анализа анализа существующих паттернов поведения.
+            </p>
+            <ul class="clients-list">
+              <li class="clients-list-item">
+                <svg width="30" height="30" class="clients-list-icon">
+                  <use href="img/sprite.svg#car"></use>
+                </svg>
+                Автомобильная химия
+              </li>
+              <li class="clients-list-item">
+                <svg width="30" height="30" class="clients-list-icon">
+                  <use href="img/sprite.svg#home"></use>
+                </svg>
+                Бытовая химия
+              </li>
+              <li class="clients-list-item">
+                <svg width="30" height="30" class="clients-list-icon">
+                  <use href="img/sprite.svg#dis"></use>
+                </svg>
+                Дезинфицирующие средства
+              </li>
+              <li class="clients-list-item">
+                <svg width="30" height="30" class="clients-list-icon">
+                  <use href="img/sprite.svg#aerozol"></use>
+                </svg>
+                Пищевые аэрозоли
+              </li>
+              <li class="clients-list-item">
+                <svg width="30" height="30" class="clients-list-icon">
+                  <use href="img/sprite.svg#cosmetic"></use>
+                </svg>
+                Косметическая продукция
+              </li>
+              <li class="clients-list-item">
+                <svg width="30" height="30" class="clients-list-icon">
+                  <use href="img/sprite.svg#brush"></use>
+                </svg>
+                Краски аэрозольные
+              </li>
+            </ul>
+          </div>
+          <!-- /.expert-content -->
+        </div>
+        <!-- /.expert-content-wrapper -->
+         <div class="expert-image">
+          <img
+          src="./img/our-production.png"
+          alt="expert"
+          class="expert-photo"
+        />
+        </div>
+        <!-- /.expert-image -->
+      </div>
+      <!-- /.container -->
+    </section>
+    <!-- /.section expert -->
