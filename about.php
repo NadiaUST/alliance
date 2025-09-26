a6aeba98a9cbd051a1903e175a7a9d413d024796
NadiaUST <ustinova7n@gmail.com>
2025-09-26 15:20:24 -0500

    create: added slider for page about

--- about.php
+++ about.php
@@ -3,11 +3,14 @@ $page_title = 'О компании';
 $header_style = 'about-company';
 $swiper_color = 'swiper-color';
 $header_margin = 'header-margin';
+$swiper_bg = 'swiper-bg';
+$buttons = 'buttons-end';
 include_once('header-page.php');
 include_once('./template-parts/expert-block.php');
 include_once('./template-parts/swiper-block.php');
 include_once('./template-parts/our-production-block.php');
 include_once('./template-parts/founder-block.php');
+include_once('./template-parts/center-block.php');
 include_once('./template-parts/clients-block.php'); ?>
 <?php include_once('footer.php');?>
 
