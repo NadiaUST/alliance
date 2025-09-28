f5f256d6135e57cca20bf82f87e87c48dc0b0237
NadiaUST <ustinova7n@gmail.com>
2025-09-28 13:31:50 -0500

    create: added link

--- template-parts/blog-block.php
+++ template-parts/blog-block.php
@@ -154,10 +154,10 @@
               <!-- /.blog-card -->
                 <ul class="pagination">
                   <li><a href="#" class="active">1</a></li>
-                  <li><a href="#">2</a></li>
-                  <li><a href="#">3</a></li>
-                  <li><a href="#">4</a></li>
-                  <li><a href="#">5</a></li>
+                  <li><a href="/more.php">2</a></li>
+                  <li><a href="/more.php">3</a></li>
+                  <li><a href="/more.php">4</a></li>
+                  <li><a href="/more.php">5</a></li>
                 </ul>
         </div>
       </div>
